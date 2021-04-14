98993081b917acad6d3f4c01db6bd76a6c6b3024
Leo <quanngovan98@gmail.com>
2021-04-14 16:30:12 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# textstyle_extension
+
+A Flutter package to implement textstyle easier
+
+## Demo
+"This is title".s14w500(color: Colors.red)
---
+++ test/textstyle_extension_test.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:textstyle_extension/textstyle_extension.dart';
+
+void main() {
+
+}
